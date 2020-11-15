cdbb09aee5cf4b020642b68176d57bba52dd1818
JunSeobKim <wnstjq616@gmail.com>
2020-11-15 16:00:57 +0900

    Initial Commit

---
+++ testSwift2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testSwift2
+//
+//  Created by 김준섭 on 2020/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
